a6afc1d9ff27cd52e4604ff2adb666209fa6e616
BP Mishra <tech@golivelabs.io>
2015-09-30 18:43:21 -0700

    fd

--- server2.js
+++ server2.js
@@ -1,11 +1,19 @@
 var express = require('express')
   , app = express();
- 
- 
+
+
+// CORS request will not work
 app.get('/items', function(req, res, next){
   res.status(200).json({message: "Items information"});
 });
 
+// working CORS request - will work from any origin
+app.get('/', function (req,res) {
+    res.set('Access-Control-Allow-Origin', '*');
+    res.json({message: 'Hello World!'});
+});
+
+// JSONP request
 app.get('/info', function (req, res) {
   console.log(req.query);
   var data = {
